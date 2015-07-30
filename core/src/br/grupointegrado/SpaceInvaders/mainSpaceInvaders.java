5053f3d70807b299a78798822fd948d9a1ee9f79
ACACIO MANFRIM COELHO <acaciocoelho@hotmail.com>
2015-07-29 22:09:36 -0300

    colocado 1

--- core/src/br/grupointegrado/SpaceInvaders/mainSpaceInvaders.java
+++ core/src/br/grupointegrado/SpaceInvaders/mainSpaceInvaders.java
@@ -15,16 +15,12 @@ public class mainSpaceInvaders extends ApplicationAdapter {
 		batch = new SpriteBatch();
 
 
-
-
-
-		
 		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
